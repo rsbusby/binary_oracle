f05f8c7387eac4fd5ea1c7fc3536aadbc5eb0973
Richard Busby <rsbusby@gmail.com>
2018-12-06 23:07:51 -0800

    endline added to serial comm

--- binary_oracle_serial_test/binary_oracle_serial_test.ino
+++ binary_oracle_serial_test/binary_oracle_serial_test.ino
@@ -32,5 +32,5 @@ void loop()
 void send_string_data_over_serial(int byte_value, int sequence_step) {
   // Send two integers representing the elements, can be decoded by receiver
   Serial.print(sequence_step);
-  Serial.print(byte_value);
+  Serial.println(byte_value);
 }
