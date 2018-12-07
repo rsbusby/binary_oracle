fa4bf18da4fafc483e96168b5f7ff079c0f446ee
Richard Busby <rsbusby@gmail.com>
2018-12-06 23:05:47 -0800

    change schema

--- binary_oracle_serial_test/binary_oracle_serial_test.ino
+++ binary_oracle_serial_test/binary_oracle_serial_test.ino
@@ -12,27 +12,25 @@ void setup() {
 void loop()
 {
 
-    // get random values from 1 to 8 for elements
-    int element_1 = random(1,9);
-    int reaction_1 = random(2);
-    int element_2 = random(1,9);
-    int reaction_2 = random(2);
-
+    // get random values 
     delay(2000);
-    send_string_data_over_serial(element_1, 0);
+    send_string_data_over_serial(1, random(2));
+    send_string_data_over_serial(1, random(2));
+    send_string_data_over_serial(3, random(2));
     delay(3000);
-    send_string_data_over_serial(reaction_1, 1);
+    send_string_data_over_serial(4, random(2));
     delay(3000);
-    send_string_data_over_serial(element_2, 2);
+
+    send_string_data_over_serial(5, random(2));
+    send_string_data_over_serial(6, random(2));
+    send_string_data_over_serial(7, random(2));
     delay(3000);
-    send_string_data_over_serial(reaction_2, 3);
+    send_string_data_over_serial(8, random(2));
 
 }
 
 void send_string_data_over_serial(int byte_value, int sequence_step) {
   // Send two integers representing the elements, can be decoded by receiver
-  Serial.print("start");
   Serial.print(sequence_step);
   Serial.print(byte_value);
-  Serial.println("end");
 }
