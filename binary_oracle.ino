a7fbbfb70c105328713345c0bd6356a7bb8a4cac
Richard Busby <rsbusby@gmail.com>
2018-12-12 19:51:33 -0800

    initialize to green

--- binary_oracle.ino
+++ binary_oracle.ino
@@ -104,11 +104,11 @@ void setup() {
   FastLED.addLeds<LED_TYPE, LED_DATA_PIN_1, COLOR_ORDER>(leds[0], NUM_LEDS);
   // FastLED.addLeds<LED_TYPE, LED_DATA_PIN_2, COLOR_ORDER>(leds[1], NUM_LEDS);
 
-  initialize_leds();
 
   // set master brightness control
   FastLED.setBrightness(100);
-  // FastLED.show();
+  initialize_leds();
+
 
   // sensor parameters
   sensor.show_sensor_value = 1;
@@ -124,7 +124,7 @@ void setup() {
 
 void initialize_leds(){
   for(int i=0;i<NUM_LEDS;i++){
-    leds[0][i] = CRGB(255,0,0);
+    leds[0][i] = CRGB(0,255,0);
   }
 }
 
